65a11592317e86f16f19ca11ceeba8c214a82584
Farzana <farzana.n.azad@gmail.com>
2017-06-10 13:30:14 -0400

    virtualpetamok

---
+++ src/virtualpetamok/Dog.java
@@ -0,0 +1,13 @@
+package virtualpetamok;
+
+public abstract class Dog extends VirtualPet {
+
+	public Dog(String nameParam, String descriptionParam) {
+		super(nameParam, descriptionParam);
+		
+	}
+	
+	public abstract void walk();
+	
+
+}
